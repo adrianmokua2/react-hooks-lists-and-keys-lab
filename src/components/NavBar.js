f005a91796bc8ded3ab548567354455c5434a35a
Adrian <adrian.mokua@icloud.com>
2023-10-28 14:06:56 +0300

    code along

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <div className="navbar">
+    {
+      links.map((link, index) => (
+        <a key={index} href={link}></a>
+      )
+
+      )
+    }
+  </div>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,6 +7,10 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        <h3>technologies</h3>
+        {technologies.map((technologies) => (
+          <span key={technologies}>{technologies}</span>
+        ))}
       </div>
     </div>
   );
